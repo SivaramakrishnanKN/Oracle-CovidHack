730c426e115fc943cb3702287bab2ccfed1a89f2
T1nkledeath <sivaram_sivaram99@yahoo.in>
2020-05-30 08:46:04 +0530

    Created Customer, Item and Service Model

--- CovidHack/CovidMart/models.py
+++ CovidHack/CovidMart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Customer:
+    name = models.TextField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    zone = models.TextField()
+    zoneID = models.IntegerField()
+    
+class Item:
+    itemName = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+class Service:
+    type = models.TextField()
+    name = models.TextField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    zone = models.TextField()
+    zoneID = models.IntegerField()
