29dadd4c648af2418144547e4c22fbad8853cfee
T1nkledeath <sivaram_sivaram99@yahoo.in>
2020-05-30 09:50:07 +0530

    Views

--- CovidHack/CovidMart/views.py
+++ CovidHack/CovidMart/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
