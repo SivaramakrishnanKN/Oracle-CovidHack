92d815f6ee36bbcb9e12c91ceef81ae412a8b006
Chetan Agarwal <chetanag35@gmail.com>
2020-05-30 09:17:10 +0530

    working models

--- CovidHack/CovidMart/admin.py
+++ CovidHack/CovidMart/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Customer, Item, Service
 # Register your models here.
+admin.register(Customer,Item,Service)(admin.ModelAdmin)
--- CovidHack/CovidMart/models.py
+++ CovidHack/CovidMart/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Customer:
+class Customer(models.Model):
     name = models.TextField()
     lat = models.FloatField()
     lon = models.FloatField()
     zone = models.TextField()
     zoneID = models.IntegerField()
     
-class Item:
+class Item(models.Model):
     itemName = models.TextField()
     price = models.FloatField()
     quantity = models.IntegerField()
 
-class Service:
-    type = models.TextField()
+class Service(models.Model):
+    type_name = models.TextField()
     name = models.TextField()
     lat = models.FloatField()
     lon = models.FloatField()
